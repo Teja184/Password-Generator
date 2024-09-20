fe2d612e196ac7c4fd8043cab89bae2be6fe03d4
Tejavardhan Reddy <150679222+Teja184@users.noreply.github.com>
2024-09-20 14:53:34 +0530

    Add files via upload

---
+++ assignment3.py
@@ -0,0 +1,10 @@
+import string
+import random
+a=string.ascii_letters+string.digits+string.punctuation
+le=int(input("enter length of password : "))
+def gen_pwd(le):
+    return random.choices(a,k=le)
+b=gen_pwd(le)
+
+print("your password is","".join(b))
+print(dir(random))
